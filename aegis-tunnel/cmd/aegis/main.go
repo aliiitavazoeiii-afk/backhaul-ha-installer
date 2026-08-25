f22bded6b8cf58be13876231d7b260362f50b6d3
aliiitavazoeiii-afk <aliiitavazoeiii@gmail.com>
2026-08-25 10:17:17 +0330

    Bump Aegis engine to 0.2.0

--- aegis-tunnel/cmd/aegis/main.go
+++ aegis-tunnel/cmd/aegis/main.go
@@ -12,7 +12,7 @@ import (
 	"github.com/aliiitavazoeiii-afk/backhaul-ha-installer/aegis-tunnel/internal/tunnel"
 )
 
-const version = "0.1.0"
+const version = "0.2.0"
 
 func main() {
 	var role, cfg string
